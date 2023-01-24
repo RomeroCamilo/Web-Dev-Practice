f7a8797746fcfd9752026f610a3ca5b2b4e21798
Camilo Romero <91925901+RomeroCamilo@users.noreply.github.com>
2023-01-24 11:22:38 -0500

    filter learned

--- scripts.js
+++ scripts.js
@@ -4,10 +4,10 @@
 //making an object of vitamins.
 let vitObject = [
     {
-        vitamin: 'Vitamin C', fecha: "2021-10-04"
+        vitamin: 'Vitamin C', fecha: "2021-10-04", id:'id1'
     },
     {
-        vitamin: 'Vitamin B', fecha: "2021-9-21"
+        vitamin: 'Vitamin B', fecha: "2021-9-21", id: 'id2'
     }
 ]
 
@@ -28,8 +28,14 @@ function addVitamin(){
     //getting date
     const fecha = document.getElementById("fecha").value
 
+    //an id for our current button. this function will return the current time in miliseconds.
+    //converting to string as we want our data to brings.
+    const id = new Date().getTime().toString()
+
     //we will now push the object. objects are surrounded by curly braces.
-    vitObject.push({vitamin: vitamin, fecha: fecha})
+    vitObject.push({vitamin: vitamin, fecha: fecha, id: id})
+
+    alert("Data has been updated, click Print Vitamins again.")
   }
 }
 
@@ -37,6 +43,30 @@ function addVitamin(){
 //function that will reset the inner html of id 'container'
 function reset(){
   document.getElementById('container').innerText = " "
+}
+
+//event will have the information of a button in the event of a specific button is clicked.
+function deleteRow(event){
+
+    //getting the id of the event button clicked/ targetting current event
+    const idToDelete = event.target.id;
+    
+    //filter vs for each: In summary, "filter" is used for filtering elements from an array based on a certain condition, 
+    //while "foreach" is used for iterating over each element and performing an action on them.
+
+    //looping through our vitObject array with filter. we want to filter out
+    //WE PUT AN EQUAL IN FILTER BECAUSE IN FILTER WE UPDATE THE ARRAY AND FILTER OUT WHAT WE DONT NEED.
+    vitObject = vitObject.filter(function(vitaminRow){
+        if(vitaminRow.id === idToDelete){
+            alert("Delete Successful")
+            return false;
+        }
+        else{
+            return true;
+        }
+    });
+
+    render();
 }
 
 
@@ -50,6 +80,7 @@ function render(){
     vitObject.forEach(function (vitaminFact){
 
         //creating a div that will be filled with value from current iteration Vitamin Fact object.
+        //this is how you create a div in javascript from html.
         const element = document.createElement('div')
 
         //adding value to the div.
@@ -58,10 +89,27 @@ function render(){
         //this how we add a class to a DOM element we created.
         element.className='vitaminFacts'
 
+        //adding a delete button to our current div element which can delete a vitamin row.
+        const deleteButton = document.createElement('button')
+       
+        deleteButton.innerText = 'Delete'
+       
+        //setting css class.
+        deleteButton.className = "deleteButtons"
+       
+        //linking the current iteration button with an id for that row.
+        deleteButton.id = vitaminFact.id
+
+        //add functionality when clicked in javascript.
+        deleteButton.onclick = deleteRow
+        
+        //add the button to our current div.
+        element.appendChild(deleteButton)
+    
         //get the div we want to append to
         const containing = document.getElementById('container')
         
-        //appending child to containing/'container'
+        //appending current div to containing/'container', our main container/div.
         containing.appendChild(element)
   })
 }
